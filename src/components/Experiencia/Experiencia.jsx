449b0542242c2095a18b0753e56bc865767331b7
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-08-15 22:19:59 -0300

    update

--- src/components/Experiencia/Experiencia.jsx
+++ src/components/Experiencia/Experiencia.jsx
@@ -4,12 +4,20 @@ import {MotionReveal} from '../../utils/Motion/MotionReveal';
 
 const experiencia = [
   {
-    empresa: 'freelancer',
+    empresa: 'Freelancer',
     ocupacao: 'Desginer Web',
     resumo: 'Trabalhei como desenvolvedor web, criando sites utilizando Shopify e Panda, plataformas similares ao WordPress. Durante esse período, utilizei minha experiência em JavaScript, HTML e CSS para implementar melhorias significativas nos projetos. Meu foco estava em personalizar e otimizar a experiência do usuário, garantindo que os sites fossem funcionais, visualmente atraentes e perfeitamente alinhados com as necessidades dos clientes.',
     tecnologias: ['HTML', 'CSS','JavaScript','Shopify'],
     local: 'Remoto',
     tempo: 'jan de 2022 - jul de 2022'
+  },
+  {
+    empresa: 'Freelancer',
+    ocupacao: 'Desenvolvedor Full Stack',
+    resumo: 'Atuei como desenvolvedor full stack em projetos personalizados, desenvolvendo aplicações web completas desde o front-end até o back-end. Utilizei React no front-end, Node.js no back-end e PostgreSQL como banco de dados, garantindo uma integração eficiente entre as camadas. Meu foco foi entregar soluções robustas e escaláveis, alinhadas às necessidades dos clientes, sempre com atenção à experiência do usuário e à performance da aplicação.',
+    tecnologias: ['HTML', 'CSS','JavaScript','React','Node.js','TypeScript','Knex','PostgreSQL'],
+    local: 'Remoto',
+    tempo: 'jan de 2024 - jul de 2024'
   },
 ]
 
